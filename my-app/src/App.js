173363bfd7d7ddbfd5ff2e9f6cd0959d9d8ecf66
Lorddeepwhite <45059291+Lorddeepwhite@users.noreply.github.com>
2019-11-20 20:51:32 +0700

    Coba update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -273,7 +273,132 @@ class App extends Component{
   }
 
   User2 = () => {
-    
+    return(
+      <Container>
+        <Form>
+          <Form.Group as={Col} controlId="formGridState">
+            <Form.Label>Select a day</Form.Label>
+            <Form.Control as="select" value={this.state.hari} onChange={this.handleChangeHari}>
+              <option value="">Choose...</option>
+              <option value="Senin">Senin</option>
+              <option value="Selasa">Selasa</option>
+              <option value="Rabu">Rabu</option>
+              <option value="Kamis">Kamis</option>
+              <option value="Jumat">Jumat</option>
+            </Form.Control>
+          </Form.Group>
+        </Form> 
+
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th style={{width:80, textAlign: "center"}}>Jam</th>
+              <th style={{textAlign: "center"}}>Konselor</th>
+              <th style={{textAlign: "center"}}>Availability</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.jadwal.map((item, index)=>(
+              <tr key={index}>
+                {/* <td>{index+1}</td> */}
+                <td>{item.jam}</td>
+                <td>{item.psikolog}</td>
+                <td style={{width:80}}>{item.availability}
+                  <Button variant="outline-primary" onClick={()=>this.showModal(item)}>Propose</Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+
+        {/* Modal */}
+        <Modal show={this.state.showModal} onHide={()=>this.hideModal()}>
+          <Modal.Header closeButton>
+            <Modal.Title>Pengajuan Jadwal</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form style={{marginTop: 30}}>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridName">
+                  <Form.Label>Nama</Form.Label>
+                  <Form.Control onChange={(event)=>this.setState({dNama:event.target.value})} 
+                  style={{marginBottom:10}} required type="text" placeholder="Enter Name" />
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridNIM">
+                  <Form.Label>NIM</Form.Label>
+                  <Form.Control onChange={(event)=>this.setState({dNIM:event.target.value})}
+                  required type="text" placeholder="Enter NIM" />
+                  <Form.Control.Feedback type="invalid">
+                    Please provide a valid NIM.
+                  </Form.Control.Feedback>
+                </Form.Group>
+              </Form.Row>
+
+              <Form.Group controlId="formGridJurusan">
+                <Form.Label>Pilih jurusan</Form.Label>
+                <Form.Control required as="select" value={this.state.dJurusan} onChange={(event)=>this.setState({dJurusan:event.target.value})}>
+                    <option value="STI">STI</option>
+                    <option value="IF">IF</option>
+                    <option value="TPSDA">TPSDA</option>
+                    <option value="MA">MA</option>
+                    <option value="EB">EB</option>
+                    <option value="EL">EL</option>
+                  </Form.Control>
+                {/* <Form.Control.Feedback type="invalid">
+                    Please provide a valid email.
+                </Form.Control.Feedback> */}
+              </Form.Group>
+{/* 
+              <Form.Group controlId="formGridPhone">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control required type="text" placeholder="08XXXXXXXXXX" />
+                <Form.Control.Feedback type="invalid">
+                    Please provide a valid phone number.
+                </Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridDay">
+                  <Form.Label>Session</Form.Label>
+                  <Form.Control required as="select">
+                    <option>Monday</option>
+                    <option>Tuesday</option>
+                    <option>Wednesday</option>
+                    <option>Thursday</option>
+                    <option>Friday</option>
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group as={Col} controlId="formGridTime">
+                  <Form.Label>Time</Form.Label>
+                  <Form.Control required as="select">
+                    <option>8.00</option>
+                    <option>9.00</option>
+                    <option>10.00</option>
+                    <option>11.00</option>
+                    <option>13.00</option>
+                    <option>14.00</option>
+                    <option>15.00</option>
+                  </Form.Control>
+                </Form.Group>
+              </Form.Row> */}
+
+              <Form.Group controlId="formGridDesc">
+                <Form.Label>Description</Form.Label>
+                <Form.Control required type="text" onChange={(event)=>this.setState({dDeskripsi:event.target.value})} 
+                  style={{marginBottom:10}} placeholder="Please give a brief description" />
+                </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" type="submit" onClick={()=>this.postDataMahasiswa()}>
+                Submit
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
+    )
   }
   
   TambahJadwal = () => {
@@ -396,8 +521,8 @@ class App extends Component{
           <Alert variant="success" style={{marginTop:10}}>
             <Alert.Heading>Selamat Datang di Layanan BK-Online!</Alert.Heading>
             <p>
-              Berikut ditampilkan jadwal per hari yang akan diperbarui tiap minggu. Silahkan melakukan pengajuan jadwal 
-              sesuai ketersediaan. 
+              Berikut ditampilkan jadwal per hari yang akan diperbarui tiap pekan. Silahkan melakukan pengajuan jadwal 
+              sesuai ketersediaan jadwal konseling. 
             </p>
             <hr />
             {/* <p className="mb-0">
